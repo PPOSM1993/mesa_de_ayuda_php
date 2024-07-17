514b0eda0f27edfa7daea95b54a11e9bcd37613d
pedroosorio <pedro.osorio04@inacapmail.cl>
2024-07-17 16:38:52 -0400

    avance

--- views/reportes_soporte.php
+++ views/reportes_soporte.php
@@ -1,7 +1,7 @@
 <?php 
     include "header.php" ;
 
-    if(isset($_SESSION['usuario']) && $_SESSION['usuario']['id'] == 1) {
+    if(isset($_SESSION['usuario']) && $_SESSION['usuario']['rol'] == 1) {
 
 ?>
 
--- views/usuarios.php
+++ views/usuarios.php
@@ -2,7 +2,7 @@
     include "header.php" ;
 
     if(isset($_SESSION['usuario']) && 
-        $_SESSION['usuario']['id'] == 1 || $_SESSION['usuario']['id'] == 2) {
+        $_SESSION['usuario']['id'] == 1 || $_SESSION['usuario']['rol'] == 2) {
 
 ?>
 <!-- Page Content -->
